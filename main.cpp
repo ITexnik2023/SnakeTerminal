058647327d42242686457d84908a23f8b134be87
ITexnik <itexnik2023@gmail.com>
2024-07-18 21:44:01 +0600

    v0.2

--- main.cpp
+++ main.cpp
@@ -5,13 +5,23 @@
 
 char playingfield[14][21]; 
 
-int i, key;
+int i, key, speed = 10, score = 0;
 int x = 9, y = 7;
 int fruit_x = 5, fruit_y = 3;
 bool isRunning = true;
 int ox,oy;
-int snake_len = 1;
+int asterisk = 0;
+int UP = 0;
+int DOWN = 1;
+int LEFT = 2;
+int RIGHT = 3;
 
+int snake_dir = UP;
+void snake_movement(void);
+
+struct  snakestail{
+	int old_x, old_y;
+} snakestail_old[216];
 
 int main(){
 	initscr();
@@ -23,34 +33,72 @@ int main(){
 		strcpy(playingfield[13], "####################");
 
 		playingfield[y][x] = 'O';
+		if (asterisk > 0) { // Отрисовка хвоста змеи
+
+			for (i = 0; i < asterisk; i++)
+
+				playingfield[snakestail_old[i].old_y][snakestail_old[i].old_x] = 'O';
+
+		}    
+
 		playingfield[fruit_y][fruit_x] = '*';
 
 		clear();
 		
+		//printw("You score:" , score, "%s\n");
 		for(i = 0; i < 14; i++){
 			printw("%s\n", playingfield[i]);
 
 		}
-		for(int r = snake_len - 2; r>=0; --i){
-			playingfield[у+1][x+1] = playingfield[y][x]
-		}
-		refresh();
-		
+				refresh();
+		halfdelay(speed);
 		key = getch();
 
 		ox = x;
 		oy = y;
-		if(key == 'w') y--;	
-		if(key == 's') y++;
-		if(key == 'a') x--;
-		if(key == 'd') x++;
+
+		switch(key){
+			case 'w': snake_dir = UP; y--;
+				break;
+			case 's': snake_dir = DOWN; y++;
+				break;
+			case 'a': snake_dir = LEFT; x--;
+				break;
+			case 'd': snake_dir = RIGHT; x++;
+				break;
+			case ERR: snake_movement();
+
+		}
+				
+		
 
 		if(playingfield[y][x] == '#'){
 			x = ox;
 			y = oy;
 		}
 
+		if (playingfield[y][x] != '#') { // Движение только если голова змеи не упирается
+
+			for (i = asterisk; i >= 0; i--) { // в решётку или хвост
+
+				snakestail_old[i + 1].old_x = snakestail_old[i].old_x;
+
+				snakestail_old[i + 1].old_y = snakestail_old[i].old_y;
+
+			}
+
+			snakestail_old[0].old_x = ox;
+
+			snakestail_old[0].old_y = oy;
+
+			}
 		if(x == fruit_x && y == fruit_y){
+			score++;
+			asterisk++;
+			fruit_x = rand() % (18 - 1) + 1;
+			fruit_y = rand() % (12 - 1) + 1;
+		}
+		if(fruit_x == x && fruit_y == y){
 			fruit_x = rand() % (18 - 1) + 1;
 			fruit_y = rand() % (12 - 1) + 1;
 		}
@@ -65,3 +113,10 @@ int main(){
 	
 }
 
+void snake_movement(){
+	if(snake_dir == UP) y--;	
+	if(snake_dir == DOWN) y++;	
+	if(snake_dir == LEFT) x--;	
+	if(snake_dir == RIGHT) x++;	
+	
+}
